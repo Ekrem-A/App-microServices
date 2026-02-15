ac0dcaa40c66ce7942b6d6db942d86ec20550fb9
Ekrem-A <ekrem.ankara@ogr.sakarya.edu.tr>
2026-02-15 16:54:54 +0300

    elkstack ile log,trace,metric izlemesi yapılıyor.. Opentelemetry ile.

--- Idendity.Api/Idendity.Infrastructure/Services/OpenTelemetryOption.cs
+++ Idendity.Api/Idendity.Infrastructure/Services/OpenTelemetryOption.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace KubernetesLessons.ServiceDefaults
+namespace Idendity.Infrastructure.Services
 {
     public class OpenTelemetryOption
     {
